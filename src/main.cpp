5dda4e34c9a8e588222913e687b7f1c41db38009
JohnnyCena123 <iamahuman1395@gmail.com>
2025-04-14 13:25:02 +0300

    a

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,25 @@
 #include <Geode/Geode.hpp>
 
 using namespace geode::prelude;
+
+class GridLayoutLayer : public CCLayer {
+
+    bool init() override {
+        if (!CCLayer::init()) return false;
+
+        auto ws = CCDirector::get()->getWinSize();
+
+        auto sl = ScrollLayer::create({ws.width, ws.height});
+        return true;
+    }
+
+    static create() {
+        auto ret = new GridLayoutLayer();
+        if (ret->init()) {
+            ret->autorelease();
+            return ret;
+        }
+        delete ret;
+        return nullptr
+    }
+};
